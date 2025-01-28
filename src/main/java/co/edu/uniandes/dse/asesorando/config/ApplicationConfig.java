84a22dd7b75707180425f5108fbbb89bb0cbd0a1
Ana Sofía Padilla Daza <78044651+aspd-2501@users.noreply.github.com>
2025-01-28 11:08:56 -0500

    add initial structure

--- src/main/java/co/edu/uniandes/dse/asesorando/config/ApplicationConfig.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package co.edu.uniandes.dse.ZZZ.config;
+package co.edu.uniandes.dse.asesorando.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
--- src/main/java/co/edu/uniandes/dse/asesorando/exceptions/ApiError.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/exceptions/ApiError.java
@@ -1,4 +1,4 @@
-package co.edu.uniandes.dse.ZZZ.exceptions;
+package co.edu.uniandes.dse.asesorando.exceptions;
 
 import java.time.LocalDateTime;
 
