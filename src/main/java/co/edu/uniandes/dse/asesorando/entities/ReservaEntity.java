ffac7da249a15605bdf6d234b131d6463251cfb2
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-02-10 08:35:09 -0500

    cambios

--- src/main/java/co/edu/uniandes/dse/asesorando/entities/ReservaEntity.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/entities/ReservaEntity.java
@@ -10,27 +10,23 @@ import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 
-@Table(name = "reserva")
-
 @Data
 @Entity
-
-
 public class ReservaEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private LocalDate fechaReserva;
 
-    @ManyToOne
-    @JoinColumn(name = "estudiante_id", nullable = false)
-    private Estudiante estudiante;
+ 
+    //@JoinColumn(name = "estudiante_id", nullable = false)
+    //private Estudiante estudiante;
 
-    @ManyToOne
-    @JoinColumn(name = "asesoria_id", nullable = false)
-    private Asesoria asesoria;
+    
+    //@JoinColumn(name = "asesoria_id", nullable = false)
+   // private Asesoria asesoria;
 
     private boolean cancelada = false;
     
