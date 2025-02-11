30ae180e6ab84aaeca5a2520bf2425cf47304698
Zair Montoya <z.montoya@uniandes.edu.co>
2025-02-11 06:39:14 -0500

    Comentario Repository

--- src/main/java/co/edu/uniandes/dse/asesorando/repositories/ComentarioRepository.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/repositories/ComentarioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ComentarioRepository extends  JpaRepository<ComentarioEntity, Long> {
+public interface ComentarioRepository extends  JpaRepository<ComentarioRepository, Long> {
     
     
 }
