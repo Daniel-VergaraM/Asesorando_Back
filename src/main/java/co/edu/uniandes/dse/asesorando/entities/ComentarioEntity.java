0fc64f971d4af12e15cd71aef007856b94ba0e5a
Zair Montoya <z.montoya@uniandes.edu.co>
2025-02-12 18:01:47 -0500

    Update Comentario Repository

--- src/main/java/co/edu/uniandes/dse/asesorando/entities/ComentarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/entities/ComentarioEntity.java
@@ -1,17 +1,24 @@
 package co.edu.uniandes.dse.asesorando.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Entity
 public class ComentarioEntity extends BaseEntity {
 
     private String comentario;
 
-    private int calificacion;
+    private Integer calificacion;
 
-    //private EstudianteEntity estudiante;
+    @OneToMany(mappedBy = "comentar")
+    private EstudianteEntity estudiante;
 
    
 }
