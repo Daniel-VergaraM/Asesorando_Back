29af7be57d6c516b75a9e2fdc4ae98e2b472a7ba
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-02-12 16:26:15 -0500

    ReservaService

--- src/main/java/co/edu/uniandes/dse/asesorando/repositories/ReservaRepository.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/repositories/ReservaRepository.java
@@ -1,4 +1,7 @@
 package co.edu.uniandes.dse.asesorando.repositories;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +11,7 @@ import co.edu.uniandes.dse.asesorando.entities.ReservaEntity;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<ReservaEntity, Long> {
-    ReservaEntity completarReserva(String estado);
-    ReservaEntity cancelarReserva(boolean cancelada);
+    List<ReservaEntity> findByfechaReserva(LocalDate fecha);
+    List<ReservaEntity> findByEstado(String estado);
+    
 }
---
+++ src/main/java/co/edu/uniandes/dse/asesorando/services/ReservaService.java
@@ -0,0 +1,57 @@
+package co.edu.uniandes.dse.asesorando.services;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.asesorando.entities.AsesoriaEntity;
+import co.edu.uniandes.dse.asesorando.entities.EstudianteEntity;
+import co.edu.uniandes.dse.asesorando.entities.ReservaEntity;
+import co.edu.uniandes.dse.asesorando.repositories.ReservaRepository;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice.Local;
+
+@Slf4j
+@Service
+
+public class ReservaService {
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+    
+        @Transactional
+        public ReservaEntity crearReserva(LocalDate fechaReserva, EstudianteEntity estudiante, AsesoriaEntity asesoria){ 
+               ReservaEntity reserva = new ReservaEntity();
+    
+               reserva.setFechaReserva(fechaReserva);
+               reserva.setEstudiante(estudiante);
+               reserva.setAsesoria(asesoria);
+    
+               ReservaEntity reservaGuardada = reservaRepository.save(reserva);
+    
+               return reservaGuardada;
+        }
+    
+        @Transactional
+        public String toString(String fechaReserva, EstudianteEntity estudiante, AsesoriaEntity asesoria) {
+            return "ReservaEntity{" +
+                "fechaReserva='" + fechaReserva + '\'' +
+                ", estudiante=" + (estudiante != null ? estudiante : "N/A") +
+                ", asesoria=" + (asesoria != null ? asesoria : "N/A") +
+                '}';
+        }
+    
+        @Transactional
+        public List<ReservaEntity> listarReservas() {
+            return reservaRepository.findAll();
+        }
+
+    
+
+    }
+
+
