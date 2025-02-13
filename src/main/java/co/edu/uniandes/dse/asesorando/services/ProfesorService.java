63496cf3463f0321bc7b02c787e550b812bb0b1c
Daniel Vergara <d.vergaram2@uniandes.edu.co>
2025-02-13 16:16:57 -0500

    Add TematicaService, TematicaProfesorService and UsuarioService implementations for managing thematic areas and users

--- src/main/java/co/edu/uniandes/dse/asesorando/services/ProfesorService.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/services/ProfesorService.java
@@ -33,12 +33,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.edu.uniandes.dse.asesorando.entities.ProfesorEntity;
+import co.edu.uniandes.dse.asesorando.entities.ProfesorPresencialEntity;
+import co.edu.uniandes.dse.asesorando.entities.ProfesorVirtualEntity;
 import co.edu.uniandes.dse.asesorando.repositories.ProfesorRepository;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Clase que implementa la conexión con el repositorio de Usuario
+ * Clase que implementa la conexión con el repositorio de Profesor
  *
  * @author Daniel-VergaraM
  */
@@ -49,11 +51,22 @@ public class ProfesorService {
     @Autowired
     private ProfesorRepository profesorRepository;
 
+    /**
+     * Metodo para registrar un profesor por medio de sus atributos base
+     *
+     * @param nombre
+     * @param correo
+     * @param contrasena
+     * @return
+     */
     @Transactional
-    public ProfesorEntity registrarProfesor(@Valid @NotNull ProfesorEntity profesor) {
-        log.info("Registrando un profesor nuevo");
+    public ProfesorEntity createProfesor(@Valid @NotNull String nombre, @Valid @NotNull String correo, @Valid @NotNull String contrasena) {
+        ProfesorEntity profesor = new ProfesorEntity();
+        profesor.setNombre(nombre);
+        profesor.setCorreo(correo);
+        profesor.setContrasena(contrasena);
 
-        Optional<ProfesorEntity> profesorExistente = profesorRepository.findById(profesor.getId());
+        Optional<ProfesorEntity> profesorExistente = profesorRepository.findByCorreo(correo);
 
         if (profesorExistente.isPresent()) {
             throw new IllegalArgumentException("El profesor ya esta registrado.");
@@ -63,36 +76,94 @@ public class ProfesorService {
         return profesorRepository.save(profesor);
     }
 
+    /**
+     * Metodo para registrar un profesor por medio de un objeto profesor
+     *
+     * @param profesor
+     * @return
+     */
     @Transactional
-    public ProfesorEntity actualizarProfesor(@Valid @NotNull ProfesorEntity profesor) {
-        log.info("Actualizando un profesor");
+    public ProfesorEntity createProfesor(@Valid @NotNull ProfesorEntity profesor) {
+        log.info("Registrando un profesor nuevo");
 
         Optional<ProfesorEntity> profesorExistente = profesorRepository.findById(profesor.getId());
 
-        if (profesorExistente.isEmpty()) {
-            throw new IllegalArgumentException("El profesor no existe.");
+        if (profesorExistente.isPresent()) {
+            log.error("El profesor ya esta registrado.");
+            throw new IllegalArgumentException("El profesor ya esta registrado.");
         }
 
-        log.info("Profesor actualizado");
+        log.info("Profesor creado");
         return profesorRepository.save(profesor);
     }
 
+    /**
+     * Metodo para actualizar un profesor por medio de su id y un objeto
+     * profesor
+     *
+     * @param id
+     * @param profesor
+     * @return
+     */
     @Transactional
-    public void eliminarProfesor(Long profesorId) {
-        log.info("Eliminando un profesor");
+    public ProfesorEntity updateProfesor(@NotNull Long id, @Valid @NotNull ProfesorEntity profesor) {
+        log.info("Actualizando un profesor");
 
-        Optional<ProfesorEntity> profesorExistente = profesorRepository.findById(profesorId);
+        ProfesorEntity profesorExistente = profesorRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("El profesor no existe."));
 
-        if (profesorExistente.isEmpty()) {
-            throw new IllegalArgumentException("El profesor no existe.");
+        profesorExistente.setNombre(profesor.getNombre());
+        profesorExistente.setCorreo(profesor.getCorreo());
+        profesorExistente.setContrasena(profesor.getContrasena());
+        profesorExistente.setTematicas(profesor.getTematicas());
+        profesorExistente.setTipo(profesor.getTipo());
+        profesorExistente.setFormacion(profesor.getFormacion());
+        profesorExistente.setExperiencia(profesor.getExperiencia());
+        profesorExistente.setPrecioHora(profesor.getPrecioHora());
+        profesorExistente.setFotoUrl(profesor.getFotoUrl());
+
+        if (profesor instanceof ProfesorVirtualEntity && profesorExistente instanceof ProfesorVirtualEntity) {
+            ((ProfesorVirtualEntity) profesorExistente)
+                    .setEnlaceReunion(((ProfesorVirtualEntity) profesor).getEnlaceReunion());
         }
 
-        log.info("Profesor eliminado");
-        profesorRepository.deleteById(profesorId);
+        if (profesor instanceof ProfesorPresencialEntity && profesorExistente instanceof ProfesorPresencialEntity) {
+            ((ProfesorPresencialEntity) profesorExistente)
+                    .setCodigoPostal(((ProfesorPresencialEntity) profesor).getCodigoPostal());
+            ((ProfesorPresencialEntity) profesorExistente)
+                    .setLatitud(((ProfesorPresencialEntity) profesor).getLatitud());
+            ((ProfesorPresencialEntity) profesorExistente)
+                    .setLongitud(((ProfesorPresencialEntity) profesor).getLongitud());
+        }
+
+        log.info("Profesor actualizado");
+        return profesorRepository.save(profesorExistente);
     }
 
+    /**
+     * Metodo para eliminar un profesor por medio de su id
+     *
+     * @param profesorId
+     */
     @Transactional
-    public ProfesorEntity obtenerProfesor(Long profesorId) {
+    public void deleteProfesor(@NotNull Long profesorId) {
+        log.info("Eliminando un profesor");
+
+        ProfesorEntity profesorExistente = profesorRepository.findById(profesorId)
+                .orElseThrow(() -> new IllegalArgumentException("El profesor no existe."));
+
+        log.info("Profesor eliminado");
+        profesorRepository.deleteById(profesorExistente.getId());
+    }
+
+    /**
+     * Metodo para obtener un profesor por medio de su id
+     *
+     * @param profesorId
+     * @return
+     */
+    @Transactional
+    public ProfesorEntity getProfesor(Long profesorId) {
         log.info("Obteniendo un profesor");
 
         Optional<ProfesorEntity> profesorExistente = profesorRepository.findById(profesorId);
@@ -105,8 +176,13 @@ public class ProfesorService {
         return profesorExistente.get();
     }
 
+    /**
+     * Metodo para obtener todos los profesores
+     *
+     * @return
+     */
     @Transactional
-    public <T extends ProfesorEntity> Iterable<T> obtenerProfesores() {
+    public <T extends ProfesorEntity> Iterable<T> getProfesores() {
         log.info("Obteniendo todos los profesores");
         Set<T> profesores = Set.of();
         profesores.addAll(profesorRepository.findByTipo("PROFESORVIRTUAL"));
@@ -116,8 +192,14 @@ public class ProfesorService {
         return profesores;
     }
 
+    /**
+     * Metodo para obtener un profesor por medio de su correo
+     *
+     * @param correo
+     * @return
+     */
     @Transactional
-    public <T extends ProfesorEntity> T obtenerProfesorPorCorreo(String correo) {
+    public <T extends ProfesorEntity> T getProfesorPorCorreo(String correo) {
         log.info("Obteniendo un profesor por correo");
         Optional<T> profesorExistente = profesorRepository.findByCorreo(correo);
         if (profesorExistente.isEmpty()) {
@@ -128,8 +210,14 @@ public class ProfesorService {
         return (T) profesorExistente.get();
     }
 
+    /**
+     * Metodo para obtener un profesor por medio de su nombre
+     *
+     * @param nombre
+     * @return
+     */
     @Transactional
-    public <T extends ProfesorEntity> T obtenerProfesorPorNombre(String nombre) {
+    public <T extends ProfesorEntity> T getProfesorPorNombre(String nombre) {
         log.info("Obteniendo un profesor por nombre");
         Optional<T> profesorExistente = profesorRepository.findByNombre(nombre);
         if (profesorExistente.isEmpty()) {
@@ -140,8 +228,14 @@ public class ProfesorService {
         return profesorExistente.get();
     }
 
+    /**
+     * Metodo para obtener un profesor por medio de su tematica
+     *
+     * @param tematica
+     * @return
+     */
     @Transactional
-    public Iterable<ProfesorEntity> obtenerProfesorPorTematica(String tematica) {
+    public Iterable<ProfesorEntity> getProfesorPorTematica(String tematica) {
         log.info("Obteniendo un profesor por tematica");
         Set<ProfesorEntity> profesores = Set.of();
         profesores.addAll(profesorRepository.findAll());
@@ -153,8 +247,14 @@ public class ProfesorService {
         return profesores;
     }
 
+    /**
+     * Metodo para obtener un profesor por medio de su tipo
+     *
+     * @param tipo
+     * @return
+     */
     @Transactional
-    public Iterable<ProfesorEntity> obtenerProfesorPorTipo(String tipo) {
+    public Iterable<ProfesorEntity> getProfesorPorTipo(String tipo) {
         log.info("Obteniendo un profesor por tipo");
         Set<ProfesorEntity> profesores = Set.of();
         profesores.addAll(profesorRepository.findByTipo(tipo));
@@ -162,8 +262,15 @@ public class ProfesorService {
         return profesores;
     }
 
+    /**
+     * Metodo para obtener un profesor por medio de su tipo y tematica
+     *
+     * @param tipo
+     * @param tematica
+     * @return
+     */
     @Transactional
-    public Iterable<ProfesorEntity> obtenerProfesorPorTipoAndTematica(String tipo, String tematica) {
+    public Iterable<ProfesorEntity> getProfesorPorTipoTematica(String tipo, String tematica) {
         log.info("Obteniendo un profesor por tipo y tematica");
         Set<ProfesorEntity> profesores = Set.of();
         profesores.addAll(profesorRepository.findByTipo(tipo));
