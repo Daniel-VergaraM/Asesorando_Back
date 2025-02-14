af0773beb8f3221555ff33e403f8fc1176511502
Juan Manuel Rojas <jm.rojasp1@uniandes.edu.co>
2025-02-14 01:42:51 -0500

    Servicios Calendario Acabados

--- src/main/java/co/edu/uniandes/dse/asesorando/entities/CalendarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/entities/CalendarioEntity.java
@@ -1,28 +1,30 @@
 package co.edu.uniandes.dse.asesorando.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import java.util.Date;
-import co.edu.uniandes.dse.asesorando.entities.AsesoriaEntity;
-import co.edu.uniandes.dse.asesorando.entities.ProfesoreEntity;
+import java.util.List;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
-
 public class CalendarioEntity extends BaseEntity 
 { 
 
 private Date fechaInicio;
 private Date fechaFin;
-@podamExclude
-@OneToOne
-private AsesoriaEntity asesoria;
-@podamExclude   
-@OneToOne
-private ProfesoreEntity profesor;
-@podamExclude
 
+@PodamExclude
+@OneToMany(mappedBy = "calendario",fetch = FetchType.LAZY)
+private List<ReservaEntity> reservas;
+@PodamExclude
+@ManyToOne(fetch = FetchType.LAZY)
+private ProfesorEntity profesor;
+@PodamExclude
+@OneToMany(mappedBy = "calendario",fetch = FetchType.LAZY)
+private List<AsesoriaEntity> asesorias;
 
 }
-
-
