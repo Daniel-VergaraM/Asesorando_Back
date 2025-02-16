4e6667eaade9b0385fb1e671e21df49e896013d0
Juan M Rojas <jm.rojasp1@uniandes.edu.co>
2025-02-16 17:12:32 -0500

    asociaciones calendario entity

--- src/main/java/co/edu/uniandes/dse/asesorando/entities/CalendarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/entities/CalendarioEntity.java
@@ -2,6 +2,7 @@ package co.edu.uniandes.dse.asesorando.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
@@ -19,12 +20,18 @@ private Date fechaFin;
 
 @PodamExclude
 @OneToMany(mappedBy = "calendario",fetch = FetchType.LAZY)
+@JoinColumn (name = "reserva")
 private List<ReservaEntity> reservas;
+
+
 @PodamExclude
 @ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn (name = "profesor")
 private ProfesorEntity profesor;
+
 @PodamExclude
 @OneToMany(mappedBy = "calendario",fetch = FetchType.LAZY)
+@JoinColumn (name = "asesoria")
 private List<AsesoriaEntity> asesorias;
 
 }
