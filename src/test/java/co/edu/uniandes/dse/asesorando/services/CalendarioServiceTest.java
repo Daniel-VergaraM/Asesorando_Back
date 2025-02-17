83ffb5bdf645c5435ad915313e5aa43b547d8c40
Juan M Rojas <jm.rojasp1@uniandes.edu.co>
2025-02-17 10:23:05 -0500

    CRUD pruebas finalizado CalendarioService

--- src/test/java/co/edu/uniandes/dse/asesorando/services/CalendarioServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/asesorando/services/CalendarioServiceTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.BeforeEach;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 
 import co.edu.uniandes.dse.asesorando.entities.CalendarioEntity;
+import co.edu.uniandes.dse.asesorando.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.asesorando.exceptions.IllegalOperationException;
 import jakarta.transaction.Transactional;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -52,6 +54,7 @@ private void insertData() {
 		CalendarioEntity result = calendarioService.createCalendario(newEntity);
         assertNotNull(result);   
         CalendarioEntity entity = entityManager.find(CalendarioEntity.class, result.getId());
+        assertEquals(newEntity.getId(), entity.getId());
         assertEquals(newEntity.getFechaInicio(), entity.getFechaInicio());
         assertEquals(newEntity.getFechaFin(), entity.getFechaFin());
     }
@@ -79,11 +82,54 @@ private void insertData() {
     }
     }
     @Test
-    void testGetCalendario(){
+    void testGetCalendario() {
     CalendarioEntity entity = calendarioList.get(0);
     CalendarioEntity resultEntity = calendarioService.getCalendario(entity.getId());
+
     assertNotNull(resultEntity);
+    assertEquals(entity.getId(), resultEntity.getId());
     assertEquals(entity.getFechaInicio(), resultEntity.getFechaInicio());
     assertEquals(entity.getFechaFin(), resultEntity.getFechaFin());
+}
+
+@Test
+void testGetInvalidCalendario() {
+    assertThrows(NoSuchElementException.class, () -> {
+        calendarioService.getCalendario(0L);
+    });
+}
+@Test
+	void testUpdateCalendario() throws EntityNotFoundException, IllegalOperationException {
+		CalendarioEntity entity = calendarioList.get(0);
+
+		calendarioService.updateCalendario(entity);
+
+		CalendarioEntity resp = entityManager.find(CalendarioEntity.class, entity.getId());
+        assertEquals(entity.getId(), resp.getId());
+		assertEquals(entity.getFechaInicio(), resp.getFechaInicio());
+        assertEquals(entity.getFechaFin(), resp.getFechaFin());
+		
+	}
+    @Test
+    void testUpdateCalendarioInvalid() {
+        assertThrows(IllegalOperationException.class, () -> {
+            CalendarioEntity entity = factory.manufacturePojo(CalendarioEntity.class);
+            entity.setFechaInicio(null);
+            entity.setFechaFin(null);
+            calendarioService.updateCalendario(entity);
+        });
+    }  
+    @Test
+    void testDeleteCalendario() throws EntityNotFoundException {
+        CalendarioEntity entity = calendarioList.get(0);
+        calendarioService.deleteCalendario(entity.getId());
+        CalendarioEntity deleted = entityManager.find(CalendarioEntity.class, entity.getId());
+        assertNull(deleted);
     }
+    @Test
+	void testDeleteInvalidCalendario(){
+		assertThrows(EntityNotFoundException.class, ()->{
+			calendarioService.deleteCalendario(0L);
+		});
+	} 
 }
--- src/test/java/co/edu/uniandes/dse/asesorando/services/PodamFactory.java
+++ src/test/java/co/edu/uniandes/dse/asesorando/services/PodamFactory.java
@@ -5,7 +5,7 @@ import co.edu.uniandes.dse.asesorando.entities.CalendarioEntity;
 public class PodamFactory {
 
     public CalendarioEntity manufacturePojo(Class<CalendarioEntity> class1) {
-        // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'manufacturePojo'");
     }
 
