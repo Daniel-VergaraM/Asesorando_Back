6a20aa46c3809eaf376c7e266d61bdfd167f9774
Juan Manuel Rojas <jm.rojasp1@uniandes.edu.co>
2025-03-03 08:34:18 -0500

    DTO y Detail DTO Calendario

--- src/main/java/co/edu/uniandes/dse/asesorando/dto/CalendarioDTO.java
+++ src/main/java/co/edu/uniandes/dse/asesorando/dto/CalendarioDTO.java
@@ -1,7 +1,12 @@
 package co.edu.uniandes.dse.asesorando.dto;
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class CalendarioDTO {
+
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
 
 }
